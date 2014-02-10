986bc1f3229b691e8c7246903469539c02a47fa7
htchoi1995 <htchoi1995@gmail.com>
2014-02-10 23:29:30 +0900

    cham 2 improve -@long

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -42,11 +42,17 @@ public class DateView extends TextView {
     private SimpleDateFormat mWeekdayFormat;
     private SimpleDateFormat mDateFormat;
     private String mLastText;
+    private boolean mScreenOn = true;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_ON)) {
+                mScreenOn = true;
+            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                mScreenOn = false;
+            }
             if (Intent.ACTION_TIME_TICK.equals(action)
                     || Intent.ACTION_TIME_CHANGED.equals(action)
                     || Intent.ACTION_TIMEZONE_CHANGED.equals(action)
@@ -56,7 +62,9 @@ public class DateView extends TextView {
                     // need to get a fresh date format
                     mDateFormat = null;
                 }
-                updateClock();
+                if (mScreenOn) {
+                	updateClock();
+                }
             }
         }
     };
@@ -74,6 +82,8 @@ public class DateView extends TextView {
         filter.addAction(Intent.ACTION_TIME_CHANGED);
         filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
         filter.addAction(Intent.ACTION_LOCALE_CHANGED);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
         mContext.registerReceiver(mIntentReceiver, filter, null, null);
 
         updateClock();
